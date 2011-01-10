0bfed4abc331bac4ebad82b5d2e83d8258c64228
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-01-10 21:03:16 +0000

    renamed some of the output to be less error-prone

--- distribution/src/main/bin/cytoscape.sh
+++ distribution/src/main/bin/cytoscape.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -Xss10M -Xmx1550M -Dswing.aatext=true -Dawt.useSystemAAFontSettings=lcd -Dapple.laf.useScreenMenuBar=true -Dapple.awt.fileDialogForDirectories=true -Dapple.awt.graphics.UseQuartz=true -Dcom.apple.mrj.application.apple.menu.about.name=Cytoscape -Dorg.ops4j.pax.logging.DefaultServiceLog.level=NONE -Dbundles.configuration.location=bundles/configurations launcher-1.0-SNAPSHOT-jar-with-dependencies.jar 
+java -jar -Xss10M -Xmx1550M -Dswing.aatext=true -Dawt.useSystemAAFontSettings=lcd -Dapple.laf.useScreenMenuBar=true -Dapple.awt.fileDialogForDirectories=true -Dapple.awt.graphics.UseQuartz=true -Dcom.apple.mrj.application.apple.menu.about.name=Cytoscape -Dorg.ops4j.pax.logging.DefaultServiceLog.level=NONE -Dbundles.configuration.location=bundles/configurations cytoscape-launcher.jar 
