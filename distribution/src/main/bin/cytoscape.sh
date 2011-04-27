95e87053a68d869c1419cc85a1301590ec5b1f61
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-04-27 23:22:12 +0000

    Updated VM options for Mac.

--- distribution/src/main/bin/cytoscape.sh
+++ distribution/src/main/bin/cytoscape.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -Xss10M -Xmx1550M -Dswing.aatext=true -Dawt.useSystemAAFontSettings=lcd -Dapple.laf.useScreenMenuBar=true -Dapple.awt.fileDialogForDirectories=true -Dapple.awt.graphics.UseQuartz=true -Dcom.apple.mrj.application.apple.menu.about.name=Cytoscape -Dorg.ops4j.pax.logging.DefaultServiceLog.level=NONE -Dbundles.configuration.location=bundles/configurations -Dfelix.fileinstall.dir=bundles/plugins cytoscape-launcher.jar 
+java -jar -Xss10M -Xmx1550M -Dawt.useSystemAAFontSettings=lcd -Dapple.laf.useScreenMenuBar=true -Dapple.awt.rendering=speed -Dapple.awt.fileDialogForDirectories=true -Dcom.apple.mrj.application.apple.menu.about.name="Cytoscape 3" -Dorg.ops4j.pax.logging.DefaultServiceLog.level=NONE -Dbundles.configuration.location=bundles/configurations -Dfelix.fileinstall.dir=bundles/plugins cytoscape-launcher.jar 
