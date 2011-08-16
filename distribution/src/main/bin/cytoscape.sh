762338f18bf2c76cf645ee75de3f9f61d59aaa39
rodche <rodche@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-08-16 19:29:37 +0000

    Added debug opts (commented out) to the sh script. Fixed "organisms" list in the combo box in the Pathway Commons plugin UI.

--- distribution/src/main/bin/cytoscape.sh
+++ distribution/src/main/bin/cytoscape.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-java -jar -Xss10M -Xmx1550M -Dawt.useSystemAAFontSettings=lcd -Dapple.laf.useScreenMenuBar=true -Dapple.awt.rendering=speed -Dapple.awt.fileDialogForDirectories=true -Dcom.apple.mrj.application.apple.menu.about.name="Cytoscape 3" -Dorg.ops4j.pax.logging.DefaultServiceLog.level=NONE -Dbundles.configuration.location=bundles/configurations -Dfelix.fileinstall.dir=bundles/plugins cytoscape-launcher.jar 
+CY3_OPTIONS="-Xss10M -Xmx1550M -Dawt.useSystemAAFontSettings=lcd -Dapple.laf.useScreenMenuBar=true -Dapple.awt.rendering=speed -Dapple.awt.fileDialogForDirectories=true -Dcom.apple.mrj.application.apple.menu.about.name="Cytoscape 3" -Dorg.ops4j.pax.logging.DefaultServiceLog.level=NONE -Dbundles.configuration.location=bundles/configurations -Dfelix.fileinstall.dir=bundles/plugins"
+
+java $CY3_OPTIONS -jar cytoscape-launcher.jar 
+
+# use this instead with a java debugger
+#java $CY3_OPTIONS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=12345 -jar cytoscape-launcher.jar
