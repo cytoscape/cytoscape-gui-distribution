53c38a55b1cbc32b21a110f910e0575ddd1bba13
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-08-17 23:32:34 +0000

    fixed shell script variable

--- distribution/src/main/bin/cytoscape.sh
+++ distribution/src/main/bin/cytoscape.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CY3_OPTIONS="-Xss10M -Xmx1550M -Dawt.useSystemAAFontSettings=lcd -Dapple.laf.useScreenMenuBar=true -Dapple.awt.rendering=speed -Dapple.awt.fileDialogForDirectories=true -Dcom.apple.mrj.application.apple.menu.about.name="Cytoscape 3" -Dorg.ops4j.pax.logging.DefaultServiceLog.level=NONE -Dbundles.configuration.location=bundles/configurations -Dfelix.fileinstall.dir=bundles/plugins"
+CY3_OPTIONS="-Xss10M -Xmx1550M -Dawt.useSystemAAFontSettings=lcd -Dapple.laf.useScreenMenuBar=true -Dapple.awt.rendering=speed -Dapple.awt.fileDialogForDirectories=true -Dcom.apple.mrj.application.apple.menu.about.name=Cytoscape -Dorg.ops4j.pax.logging.DefaultServiceLog.level=NONE -Dbundles.configuration.location=bundles/configurations -Dfelix.fileinstall.dir=bundles/plugins"
 
 java $CY3_OPTIONS -jar cytoscape-launcher.jar 
 
