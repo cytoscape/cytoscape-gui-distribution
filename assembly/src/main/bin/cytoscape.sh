71c86a878ad52281e57a9102058e747ceb030e3c
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-08 19:27:08 +0000

    Fixed launcher script so it allows user to turn on Karaf's "debug" mode

--- assembly/src/main/bin/cytoscape.sh
+++ assembly/src/main/bin/cytoscape.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 
 export JAVA_MAX_MEM=1550M
 
-export JAVA_DEBUG_OPTS="Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=${DEBUG_PORT}"
+export JAVA_DEBUG_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=${DEBUG_PORT}"
 export KARAF_OPTS="-Xss10M -splash:CytoscapeSplashScreen.png"
 
-framework/bin/karaf client "$@"
+framework/bin/karaf "$@"
