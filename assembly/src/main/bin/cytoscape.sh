20e10ffc15aea714707ebbbb22d36e062be7472e
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-17 20:57:28 +0000

    Dock icon for Mac added.

--- assembly/src/main/bin/cytoscape.sh
+++ assembly/src/main/bin/cytoscape.sh
@@ -7,6 +7,6 @@ cd $(dirname $0)
 export JAVA_MAX_MEM=1550M
 
 export JAVA_DEBUG_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=${DEBUG_PORT}"
-export KARAF_OPTS="-Xss10M -splash:CytoscapeSplashScreen.png"
+export KARAF_OPTS="-Xss10M -splash:CytoscapeSplashScreen.png -Xdock:icon=cytoscape_logo_512.png"
 
 framework/bin/karaf "$@"
