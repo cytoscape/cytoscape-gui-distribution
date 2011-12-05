d134d89f0cb0fcb1fd05e13a115dacce560f1418
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-12-05 20:42:17 +0000

    Modified cytoscape.sh so that the -Xdock option is passed to the JVM only if running on a Mac

--- assembly/src/main/bin/cytoscape.sh
+++ assembly/src/main/bin/cytoscape.sh
@@ -7,6 +7,9 @@ cd $(dirname $0)
 export JAVA_MAX_MEM=1550M
 
 export JAVA_DEBUG_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=${DEBUG_PORT}"
-export KARAF_OPTS="-Xss10M -splash:CytoscapeSplashScreen.png -Xdock:icon=cytoscape_logo_512.png"
+if [ `uname` = "Darwin" ]; then
+	CYTOSCAPE_MAC_OPTS="-Xdock:icon=cytoscape_logo_512.png"
+fi
+export KARAF_OPTS="-Xss10M -splash:CytoscapeSplashScreen.png $CYTOSCAPE_MAC_OPTS"
 
 framework/bin/karaf "$@"
