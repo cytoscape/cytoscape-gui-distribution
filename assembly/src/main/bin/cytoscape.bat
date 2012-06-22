89d78eed8b8d7c3b0d728b57c41d14e82f19f837
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-06-22 20:59:34 +0000

    Fixes #1162: Escaped backslashes and spaces when setting user.dir

--- assembly/src/main/bin/cytoscape.bat
+++ assembly/src/main/bin/cytoscape.bat
@@ -7,7 +7,7 @@ set JAVA_MAX_MEM=1550M
 
 set JAVA_DEBUG_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=%DEBUG_PORT%
 set PWD=%~dp0
-set KARAF_OPTS=-Xss10M -Duser.dir=%PWD% -splash:CytoscapeSplashScreen.png
+set KARAF_OPTS=-Xss10M -Duser.dir="%PWD:\=\\%" -splash:CytoscapeSplashScreen.png
 
 if not "X%JAVA_HOME%"==X goto TryJDKEnd
 goto :TryJRE
