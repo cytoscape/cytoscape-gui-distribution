8faafa3661487e7ef95afcb95eec0de81ec79141
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-10-04 00:13:57 +0000

    Patched to fix Config directory problem.

--- launcher/src/main/java/org/cytoscape/launcher/internal/Launcher.java
+++ launcher/src/main/java/org/cytoscape/launcher/internal/Launcher.java
@@ -1,5 +1,7 @@
 package org.cytoscape.launcher.internal;
 
+import java.io.File;
+
 import org.apache.karaf.main.Main;
 
 public class Launcher {
@@ -7,9 +9,16 @@ public class Launcher {
 
 	public static void main(String[] args) throws Exception {
 		startupArguments = args;
+		createConfigurationDirectory();
 		Main.main(args);
 	}
 	
+	private static void createConfigurationDirectory() {
+		String userHome = System.getProperty("user.home");
+		File karafData = new File(userHome, String.format("%s%s%s", "CytoscapeConfiguration", File.separator, "3"));
+		karafData.mkdirs();
+	}
+
 	public static String[] getStartupArguments() {
 		String[] result = new String[startupArguments.length];
 		System.arraycopy(startupArguments, 0, result, 0, startupArguments.length);
