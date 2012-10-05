aff9a9a20af3f691e07e225b6d9e20085e480fd5
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-10-05 22:00:44 +0000

    Fixes #1499: Launcher no longer dies when run in a read-only directory

--- launcher/src/main/java/org/cytoscape/launcher/internal/Launcher.java
+++ launcher/src/main/java/org/cytoscape/launcher/internal/Launcher.java
@@ -9,13 +9,35 @@ public class Launcher {
 
 	public static void main(String[] args) throws Exception {
 		startupArguments = args;
+		setDefaultSystemProperties();
 		createConfigurationDirectory();
 		Main.main(args);
 	}
 	
+	private static void setDefaultSystemProperties() {
+		String userHome = System.getProperty("user.home");
+		if (System.getProperty("karaf.data") == null) {
+			System.setProperty("karaf.data", join(File.separator, userHome, "CytoscapeConfiguration", "3", "karaf_data"));
+		}
+	}
+
+	private static String join(String separator, String... parts) {
+		StringBuilder builder = new StringBuilder();
+		boolean isFirst = true;
+		for (String part : parts) {
+			if (!isFirst) {
+				builder.append(separator);
+			} else {
+				isFirst = false;
+			}
+			builder.append(part);
+		}
+		return builder.toString();
+	}
+	
 	private static void createConfigurationDirectory() {
 		String userHome = System.getProperty("user.home");
-		File karafData = new File(userHome, String.format("%s%s%s", "CytoscapeConfiguration", File.separator, "3"));
+		File karafData = new File(join(File.separator, userHome, "CytoscapeConfiguration", "3"));
 		karafData.mkdirs();
 	}
 
