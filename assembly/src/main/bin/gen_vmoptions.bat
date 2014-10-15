851fd3e1044ace2e2160fbd251da5cfdddd2b5c8
Tim Hull <tmhull@ucsd.edu>
2014-10-15 16:30:41 -0700

    Re-add 32-bit builds and support to the install4j and gen_vmoptions scripts. Use -XX:+AggressiveHeap to get highest possible heap size on 32-bit platforms. Refs #2879

--- assembly/src/main/bin/gen_vmoptions.bat
+++ assembly/src/main/bin/gen_vmoptions.bat
@@ -2,33 +2,44 @@
 
 :: Generates the Cytoscape.vmoptions file
 
-:: Create the .cytoscape directory if it doesn't already exist
-;if exist "%HOMEPATH%\.cytoscape" goto dot_cytoscape_exists
-;mkdir "%HOMEPATH%\.cytoscape"
-;:dot_cytoscape_exists
+:Javatest
+	if exist findstr.out del findstr.out
+	java -version 2>&1 | findstr /I 64-Bit > findstr.out
+	if %ERRORLEVEL% EQU 0 GOTO 64bit
+	java -version 2>&1 | findstr /i Java > findstr.out
+	IF %ERRORLEVEL% EQU 0 GOTO 32bit
+	goto Nojava
 
-set physmem=768
-set minmem=768
-set maxmem=768
-if exist findmem.out del findmem.out
-systeminfo | find "Total Physical Memory" > findmem.out
-if %ERRORLEVEL% NEQ 0 GOTO setVmoptions
-for /f "tokens=4" %%i in (findmem.out) do set physmem=%%i
-set physmem=%physmem:,=%
-
-if %physmem% GTR 1535 set maxmem=1024
-if %physmem% GTR 2047 set maxmem=1536
-if %physmem% GTR 3071 (
-	set /a minmem=2048
-	set /a maxmem=%physmem%-1024
-) else (
-	set /a minmem=%maxmem%
-)
-
-:setVmoptions
+:64bit
+	set physmem=768
+	set minmem=768
+	set maxmem=768
+	if exist findmem.out del findmem.out
+	systeminfo | find "Total Physical Memory" > findmem.out
+	if %ERRORLEVEL% NEQ 0 GOTO setVmoptions
+	for /f "tokens=4" %%i in (findmem.out) do set physmem=%%i
+	set physmem=%physmem:,=%
+	
+	if %physmem% GTR 1535 set maxmem=1024
+	if %physmem% GTR 2047 set maxmem=1536
+	if %physmem% GTR 3071 (
+		set /a minmem=2048
+		set /a maxmem=%physmem%-1024
+	) else (
+		set /a minmem=%maxmem%
+	)
+	:setVmoptions
 		echo -Xms%minmem%M>Cytoscape.vmoptions
 		echo -Xmx%maxmem%M>>Cytoscape.vmoptions
+		del findmem.out
+	goto End
+ 	
+:32bit
+	echo -XX:+AggressiveHeap>Cytoscape.vmoptions
 	goto End
 
+:Nojava
+	echo ERROR: Can't find java executable
+
 :End
-	del findmem.out
+	del findstr.out
--- assembly/src/main/bin/gen_vmoptions.sh
+++ assembly/src/main/bin/gen_vmoptions.sh
@@ -10,32 +10,35 @@ if [ ! -e $vm_options_path ]; then
     /bin/mkdir $vm_options_path
 fi
 
-# Determine amount of physical memory present:
-if [ `uname` = "Darwin" ]; then
-    phys_mem=`sysctl -a | grep 'hw.memsize =' | sed 's/[ ][ ][ ]*/ /g' | cut -f 3 -d ' '`
-    phys_mem=$((phys_mem / 1024 / 1024)) # Convert from B to MiB
-else # We assume Linux
-    phys_mem=`cat /proc/meminfo | grep 'MemTotal:' | sed 's/[ ][ ][ ]*/ /g' | cut -f 2 -d ' '`
-    phys_mem=$((phys_mem / 1024)) # Convert from KiB to MiB
+if `java -version 2>&1 | grep -- 64-Bit > /dev/null`; then # We have a 64 bit JVM.
+	# Determine amount of physical memory present:
+	if [ `uname` = "Darwin" ]; then
+	    phys_mem=`sysctl -a | grep 'hw.memsize =' | sed 's/[ ][ ][ ]*/ /g' | cut -f 3 -d ' '`
+	    phys_mem=$((phys_mem / 1024 / 1024)) # Convert from B to MiB
+	else # We assume Linux
+	    phys_mem=`cat /proc/meminfo | grep 'MemTotal:' | sed 's/[ ][ ][ ]*/ /g' | cut -f 2 -d ' '`
+	    phys_mem=$((phys_mem / 1024)) # Convert from KiB to MiB
+	fi
+	
+	# Now we know the amount of physical memory, but we don't want to try to use it all
+	minmem=768
+	maxmem=768
+	
+	if [ $phys_mem -gt 3071 ]; then
+		minmem=2048 
+	    maxmem=$((phys_mem-1024))
+	else
+		if [ $phys_mem -gt 1535 ]; then 
+	    	maxmem=1024
+		elif [ $phys_mem -gt 2047 ]; then 
+	    	maxmem=1536
+	    fi
+	    minmem=${maxmem}
+	fi
+	
+	echo "-Xms"${minmem}M> "$vm_options_path/Cytoscape.vmoptions"
+	echo "-Xmx"${maxmem}M>> "$vm_options_path/Cytoscape.vmoptions"
+else # Assume a 32 bit JVM.
+	echo "-XX:+AggressiveHeap"> "$vm_options_path/Cytoscape.vmoptions"
 fi
-
-# Now we know the amount of physical memory, but we don't want to try to use it all
-minmem=768
-maxmem=768
-
-if [ $phys_mem -gt 3071 ]; then
-	minmem=2048 
-    maxmem=$((phys_mem-1024))
-else
-	if [ $phys_mem -gt 1535 ]; then 
-    	maxmem=1024
-	elif [ $phys_mem -gt 2047 ]; then 
-    	maxmem=1536
-    fi
-    minmem=${maxmem}
-fi
-
-echo "-Xms"${minmem}M> "$vm_options_path/Cytoscape.vmoptions"
-echo "-Xmx"${maxmem}M>> "$vm_options_path/Cytoscape.vmoptions"
-
 exit 0
