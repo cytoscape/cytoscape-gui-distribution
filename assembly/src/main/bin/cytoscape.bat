e8c3474d7726d3fc1b0a9c88a649d459f19d19a2
David Otasek <dotasek.dev@gmail.com>
2020-01-31 15:51:00 -0800

    Update version to 3.8.0

--- assembly/src/main/bin/cytoscape.bat
+++ assembly/src/main/bin/cytoscape.bat
@@ -7,7 +7,7 @@
 :: If it's set, we dump startup information. Feel free to add startup
 :: information as appropriate.
 
-set CYTOSCAPE_VERSION=Cytoscape version: 3.8.0-SNAPSHOT
+set CYTOSCAPE_VERSION=Cytoscape version: 3.8.0
 
 set help=false
 IF "%1"=="-h" set help=true
