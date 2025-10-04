b26a7170c104285f93526defa2a594ee0a977196
Scooter Morris <scooter@cgl.ucsf.edu>
2025-10-03 17:14:16 -0700

    JDK21

--- integration-test/src/test/java/org/cytoscape/session/BasicIntegrationTest.java
+++ integration-test/src/test/java/org/cytoscape/session/BasicIntegrationTest.java
@@ -48,7 +48,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.group.CyGroupManager;
